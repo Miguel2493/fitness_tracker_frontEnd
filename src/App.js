44fd42992584737c13cf7fce95b69ce96fd4945b
codingzilla <aj_chavez@sbcglobal.net>
2021-09-16 20:18:04 -0400

    added Login router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Switch } from "react-router";
 import "./App.css";
+import { Login } from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
