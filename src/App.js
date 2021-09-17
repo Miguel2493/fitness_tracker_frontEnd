350454c975eaa71d1775fef29559009b13584e8e
codingzilla <aj_chavez@sbcglobal.net>
2021-09-16 22:28:40 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from "react-router";
 import "./App.css";
-import { Login, Register } from "./components";
+import { Login, Register, Home } from "./components";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/home" component={Home} />
       </Switch>
     </div>
   );
