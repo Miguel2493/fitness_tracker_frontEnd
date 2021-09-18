4ed60024119aa12ecd558c3eb509c18f6af11506
Miguel2493 <a2493m@gmail.com>
2021-09-18 11:57:19 -0500

    Updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //we need to check if we have a token
 //if we do, we want to hit the "me" route (fetch me)
 //no need to create a "me" component
 
 const Home = () => {
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [userName, setUser] = useState("");
+
+  useEffect(() => {
+    fetch("http://fitnesstrac-kr.herokuapp.com/api/users/me", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setUser(result.username);
+      })
+      .catch(console.error);
+  }, [token]);
+
   return (
     <div>
-      <h1>Welcome to your Fitness Tracker app!</h1>
+      <h1>Welcome to your Fitness {userName} Tracker app!</h1>
     </div>
   );
 };
---
+++ src/components/Routines.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+//we need to check if we have a token
+//if we do, we want to hit the "me" route (fetch me)
+//no need to create a "me" component
+
+const Routines = () => {
+  return (
+    <div>
+      <h1>Welcome to your Fitness Tracker app!</h1>
+    </div>
+  );
+};
+
+export default Routines;
